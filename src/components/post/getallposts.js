f882e3acebd9182ec711480524eb9a727c4bb57a
Holly Whitelaw <hollywhitelaw94@outlook.com>
2024-01-05 17:47:05 +0000

    fixed issue with auth

--- src/components/post/getallposts.js
+++ src/components/post/getallposts.js
@@ -1,6 +1,15 @@
 // get all posts 
 
-function getAllPosts(token) {
+function getAllPosts() {
+
+  const token = localStorage.getItem('token'); 
+
+  if (!token) {
+    console.error('Token not found in local storage');
+  }
+
+  console.log('Token:', token);
+
     return fetch("http://localhost:5000/api/auth/posts/all", {
         headers: {
           'Authorization': `Bearer ${token}`
